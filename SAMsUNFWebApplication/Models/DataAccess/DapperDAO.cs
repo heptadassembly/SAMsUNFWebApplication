9c1892707e8929869673e166cf281016ccd19a7e
harrisontim7104 <harrisontim7104@gmail.com>
2015-10-24 15:41:02 -0400

    added profile

---
+++ SAMsUNFWebApplication/Models/DataAccess/DapperDAO.cs
@@ -0,0 +1,42 @@
+﻿using System; 
+using System.Collections.Generic; 
+using MySql.Data.MySqlClient; 
+using System.Linq; 
+using System.Text; 
+using System.Threading.Tasks;
+using Dapper;
+
+
+namespace SAMsUNFWebApplication.Models.DataAccess
+{
+
+        public class UserRepository
+     { 
+         private MySqlConnection _openConnection; 
+ 
+ 
+         public UserRepository(MySqlConnection openConnection)
+         { 
+             this._openConnection = openConnection; 
+         } 
+ 
+ 
+         public async Task<Profile> GetByUserName(string userName)
+         { 
+             // Read the user by their username in the database. 
+             var results = await this._openConnection.QueryAsync<Profile>(@" 
+ SELECT  
+ UserId, 
+ UserName, 
+ PasswordHash 
+ FROM  
+ user 
+ WHERE UserName = @UserName 
+ ", new { UserName = userName }); 
+ 
+ 
+             var result = results.FirstOrDefault(); 
+             return result; 
+        } 
+     } 
+}
---
+++ SAMsUNFWebApplication/Models/Profile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SAMsUNFWebApplication.Models
+{
+    public class Profile
+    {
+
+        
+        public int ProfileId { get; set; }
+        public int FirstName { get; set; }
+        public int MiddleName { get; set; }
+        public int LastName { get; set; }
+        public int UserID { get; set; }
+        public int Password { get; set; }
+
+    }
+}
