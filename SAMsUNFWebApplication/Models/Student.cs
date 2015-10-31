2abc33bce188db3a3a61a36449933dd9b0e69169
harrisontim7104@gmail.com <harrisontim7104@gmail.com>
2015-10-30 21:07:34 -0400

    sync Studnent moidel, dashboard to  new  DB schema

--- SAMsUNFWebApplication/Models/Student.cs
+++ SAMsUNFWebApplication/Models/Student.cs
@@ -7,10 +7,16 @@ namespace SAMsUNFWebApplication.Models
 {
     public class Student
     {
-        public string StudentID { get; set; }
-        public string FirstName { get; set; }
-        public string MiddleName { get; set; }
-        public string LastName { get; set; }
-     
+        public int student_id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public int school_year_id { get; set; }
+        public int homeroom_id { get; set; }
+        public string grade_id { get; set; }
+        public string create_user { get; set; }
+        public DateTime create_dt { get; set; }
+        public string last_update_user { get; set; }
+        public DateTime last_update_dt { get; set; }
+        public Boolean is_deleted { get; set; }
     }
 }
