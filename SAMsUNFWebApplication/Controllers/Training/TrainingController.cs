be25140b4cc3227201e34f7f1a9eafef48320a8f
kellykayla49 <kellykayla49@gmail.com>
2015-10-31 18:17:58 -0400

    Added initial training MVC

---
+++ SAMsUNFWebApplication/Controllers/Training/TrainingController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SAMsUNFWebApplication.Models;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+using SAMsUNFWebApplication.Models.DataAccess;
+using System.IO;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text;
+
+namespace SAMsUNFWebApplication.Controllers.Training
+{
+    public class TrainingController : Controller
+    {
+        // GET: Training
+        public ActionResult Training()
+        {
+            return View();
+        }
+     }
+}
