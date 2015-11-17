f3ff19d67528298d5f2fd4a2e17fbaedcfa6a7d8
David Kolln <dkolln@gmail.com>
2015-11-16 23:42:41 -0500

    bringing contact controller back in.

---
+++ SAMsUNFWebApplication/Controllers/Contact/ContactController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CsvHelper;
+using SAMsUNFWebApplication.Models;
+using System.IO;
+
+namespace SAMsUNFWebApplication.Controllers.Contact
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ImportContact(HttpPostedFileBase file)
+        {
+            string path = null;
+            List<CSVContacts> ContactsToDisplay = new List<CSVContacts>();
+
+            try
+            {
+                if (file.ContentLength > 0)
+                {
+                    var fileName = Path.GetFileName(file.FileName);
+                    path = AppDomain.CurrentDomain.BaseDirectory + "upload\\" + fileName;
+                    file.SaveAs(path);
+
+                    using (var sr = new StreamReader(path))
+                    {
+                        var reader = new CsvReader(sr);
+                        var records = reader.GetRecords<CSVContacts>().ToList();
+
+                        return View(records);
+                    }
+                }
+            }
+            catch
+            {
+                return View();
+            }
+
+            return View();
+
+        }
+        [HttpPost]
+        public ActionResult ImportStudent(HttpPostedFileBase file)
+        {
+            string path = null;
+            List<CSVStudent> StudentsToDisplay = new List<CSVStudent>();
+
+            try
+            {
+                if (file.ContentLength > 0)
+                {
+                    var fileName = Path.GetFileName(file.FileName);
+                    path = AppDomain.CurrentDomain.BaseDirectory + "upload\\" + fileName;
+                    file.SaveAs(path);
+
+                    using (var sr = new StreamReader(path))
+                    {
+                        var reader = new CsvReader(sr);
+                        var records = reader.GetRecords<CSVStudent>().ToList();
+
+                        return View(records);
+                    }
+                }
+            }
+            catch
+            {
+                return View();
+            }
+
+            return View();
+
+        }
+        
+    }
+}
